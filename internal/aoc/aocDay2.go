c332c716badd0964cd434f81468aad01c250e07a
theBvrtosz <bartosz.gejgal@gmail.com>
2023-12-03 13:09:16 +0100

    adding day 2 code. refactoring main

---
+++ internal/aoc/aocDay2.go
@@ -0,0 +1,69 @@
+package aoc
+
+import (
+	"math"
+	"strconv"
+	"strings"
+)
+
+func parseGamesFromInput(input []string) map[int]map[string]float64 {
+	parsedGames := make(map[int]map[string]float64)
+	for _, game := range input {
+		gameSplitted := strings.Split(game, ":")
+		gameId, _ := strconv.Atoi(strings.ReplaceAll(gameSplitted[0], "Game ", ""))
+		cubeReveals := strings.ReplaceAll(gameSplitted[1], ";", ",")
+		parsedGames[gameId] = make(map[string]float64)
+		for _, reveal := range strings.Split(cubeReveals, ",") {
+			revealSplitted := strings.Split(strings.TrimSpace(reveal), " ")
+			cubesCount, _ := strconv.Atoi(revealSplitted[0])
+			cubesColour := revealSplitted[1]
+			currentCubesCount, ok := parsedGames[gameId][cubesColour]
+			if ok {
+				parsedGames[gameId][cubesColour] = math.Max(float64(cubesCount), float64(currentCubesCount))
+			} else {
+				parsedGames[gameId][cubesColour] = float64(cubesCount)
+			}
+		}
+	} 
+	return parsedGames
+}
+
+func getCubesCountForCubeColour(cubesPlayed map[string]float64, cubeColour string) float64 {
+	var currectCubeCount float64
+	if cubeCount, ok := cubesPlayed[cubeColour]; ok {
+		currectCubeCount = cubeCount
+	} else {
+		currectCubeCount = 1.0
+	}
+	return currectCubeCount
+}
+
+func Day2Part1(isTestRun bool) int {
+	input := readDayInput("2", isTestRun)
+	parsedInput := parseGamesFromInput(input)
+	redCubesCount := 12.0
+	greenCubesCount := 13.0
+	blueCubesCount := 14.0
+	gameIdSum := 0
+	for gameId, maxCubesPlayed := range parsedInput{
+		if maxCubesPlayed["red"] <= redCubesCount &&
+		maxCubesPlayed["green"] <= greenCubesCount &&
+		maxCubesPlayed["blue"] <= blueCubesCount {
+			gameIdSum = gameIdSum + gameId
+		}
+	}
+	return gameIdSum
+}
+
+func Day2Part2(isTestRun bool) int {
+	input := readDayInput("2", isTestRun)
+	parsedInput := parseGamesFromInput(input)
+	sumOfGamesPower := 0
+	for _, maxCubesPlayed := range parsedInput {
+		greenCubeCount := getCubesCountForCubeColour(maxCubesPlayed, "green")
+		blueCubeCount := getCubesCountForCubeColour(maxCubesPlayed, "blue")
+		redCubeCount := getCubesCountForCubeColour(maxCubesPlayed, "red")
+		sumOfGamesPower = sumOfGamesPower + int(greenCubeCount*blueCubeCount*redCubeCount)
+	}
+	return sumOfGamesPower
+}
--- main.go
+++ main.go
@@ -2,13 +2,30 @@ package main
 
 import (
 	"fmt"
+	"sort"
 
 	"aoc.thebvrtosz.net/internal/aoc"
 )
 
+var dayMap = map[int][2]func(bool) int {
+	1: {aoc.Day1Part1, aoc.Day1Part2},
+	2: {aoc.Day2Part1, aoc.Day2Part2},
+}
+
+func printDaySolution(dayNumber int, isTestRun bool) {
+	fmt.Printf("day %v:\n", dayNumber)
+	fmt.Printf("\t part1 solution: %v\n", dayMap[dayNumber][0](isTestRun))
+	fmt.Printf("\t part2 solution: %v\n", dayMap[dayNumber][1](isTestRun))
+	fmt.Println("--------------------")
+}
+
 func main() {
-	day1Part1Solution := aoc.Day1Part1(false)
-	day1Part2Solution := aoc.Day1Part2(false)
-	fmt.Println(day1Part1Solution)
-	fmt.Println(day1Part2Solution)
+	days := make([]int, 0)
+	for day := range dayMap {
+		days = append(days, day)
+	}
+	sort.Ints(days)
+	for _, day := range days {
+		printDaySolution(day, false)
+	}
 }
